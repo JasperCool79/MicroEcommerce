6b92750cf454a01793dcfb0744e82b05c5be5bf3
JasperCool79 <jaspercool.ygn@gmail.com>
2021-06-04 15:28:28 +0630

    update cores

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Origin',  '*');
+header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Headers', 'Accept, Content-Type, X-Auth-Token, Origin, Authorization');
 
 use Illuminate\Http\Request;
 
