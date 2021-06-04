d395393ec68379ab9cb42023f169839100547747
JasperCool79 <jaspercool.ygn@gmail.com>
2021-06-04 14:54:48 +0630

    Update CORS

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
+
+use Illuminate\Http\Request;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-CSRF-Token');
 header('Access-Control-Allow-Credentials: true');
 
-use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
