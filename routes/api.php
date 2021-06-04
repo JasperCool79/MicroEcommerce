0262e1fa018cccb3ab11e8716a3389644e5c49ed
JasperCool79 <jaspercool.ygn@gmail.com>
2021-06-04 19:56:52 +0630

    update .htaccess

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,5 @@
 <?php
 
-
-header('Access-Control-Allow-Origin',  '*');
-header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers', 'Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-
 use Illuminate\Http\Request;
 
 
